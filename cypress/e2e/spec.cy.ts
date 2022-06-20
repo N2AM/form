604b87ce16adbbaa3ba29ca3a36d1dfdfbc1e41c
N2am <nouranmahmoud321@gmail.com>
2022-06-20 10:49:18 +0200

    test: cypress testing signup flow

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,8 +1,22 @@
 describe('Signup Test', () => {
-  it('Visits Signup page', () => {
+  before(() => {
     cy.visit('/')
+  })
+
+  it('Visits Signup page', () => {
     cy.contains('Welcome to FedEx Let’s get started ...')
     cy.contains('Enter your contact information. Already have an account? log in.')
     cy.contains("Let's have an account!")
+  })
+  it('Validate successful signup', () => {
+    cy.fixture('user').then((userTestdata) => {
+      cy.get('#firstName').type(userTestdata.firstName)
+      cy.get('#lastName').type(userTestdata.lastName)
+      cy.get('#email').type(userTestdata.email)
+      cy.get('#password').type(userTestdata.password)
+      cy.get('#submitBtn').click()
+      cy.get('#confirmBtn').click()
+    })
+
   })
 })
